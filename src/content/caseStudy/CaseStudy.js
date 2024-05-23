c6dde73dd12fb0c0b2d8f356be686cd47c710276
NITISH YADAV <101405119+thisisnitishyadav@users.noreply.github.com>
2024-05-23 17:31:26 +0530

    nitish

---
+++ src/content/caseStudy/CaseStudy.js
@@ -0,0 +1,186 @@
+'use client'
+import React from 'react'
+
+const CaseStudy = () => {
+  return (
+    <div className="min-h-screen max-w-screen bg-[#191919]">
+
+{/* --------------------------------------------------------------------------------------------------- */}
+      <div className="relative bg-gradient-to-b from-black to-transparent via-black-opacity-68 via-black-opacity-46 via-black-opacity-22">
+
+      <img src="https://s3-alpha-sig.figma.com/img/9c40/58c8/5a452021c93f803d94fef71c0d0a7c88?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jRD4dJp2bvB-oSC2sEq5d4XRwNFUuFkfZQSD989vxg8qRfe3x1NW1CbpgFX5ZSepLICp3nEZCNzsrwGoN2SBF~XTTm3tWmN16oB7cMVaLn9TJ4CMrUGJ4s4PUrXueMeptBcWTneciT332Bvior5ZVG1Icj6S6ZUusZ2qIAf4CWKqUaTXQQzZ~u2MelnuXrQrV8G46iM2YyP8G6vNfnYnAsO6AxcM41zVvpNCEYFGHDt7IFWBSltpeD2fFAeuGj4yJoUCb1pCZMOZOzLE8lA1fP1bKNzeneBcmCSeYbvO9JUbvMEDTa1xrHA1HKPkdJQIpJvCNGmaUOrfqUCZEUcl5Q__" 
+           alt="" 
+           className="md:h-[520px] w-screen brightness-50" ></img>
+  
+      <div className="md:w-[750px] md:h-[210px] mt-12 md:mt-[294px] md:ml-[40px] mx-8 absolute inset-0 space-y-2">
+        <div className="text-white">
+        <p className="text-[#FF7F0E]">LATEST BLOGS</p>
+        <p className="font-inter text-2xl md:text-4xl font-extrabold leading-80 text-left">Case Study</p>
+        <p className="font-inter text-cabin md:font-bold leading-6 text-left ">TECH PYRO is a tech hiring platform that helps recruiters and engineering managers
+        effortlessly hire the best developers thanks to a powerful site of virtual recruting tools that help identify,assess,
+        interview and engage developers.</p>
+        </div>
+      </div>
+
+      </div>
+
+{/* ------------------------------------------------------------------------------------------------------------- */}
+<div className=" bg-[#191919] min-h-[680px]">
+   
+   <div className='flex justify-center mt-12 mx-8'>
+   <div className=' md:w-[898px] md:h-[131px]'>
+    <p className="text-[#FFFFFF] font-inter text-2xl font-bold leading-14 tracking-tighter text-center">Lorem ipsum dolor sit amet,consectetur</p>
+    <p className="text-[#FFFFFF] font-inter text-base font-medium leading-7 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. A tempora exercitationem pariatur consequuntur ipsam. Fugit sed commodi illum placeat, voluptatem, impedit animi omnis deleniti neque autem a amet nisi odio perspiciatis itaque iste ea?</p>
+   </div>
+   </div>
+
+   <div className="md:flex justify-center gap-4 space-y-4 my-4 mx-8 ">
+
+   <div className="space-y-4 md:w-1/3">
+   <div className="text-white md:text-right space-y-2">
+    <p className='flex items-center justify-center rounded-full bg-[#FFFFFF] md:ml-[210px] h-[54px] w-[54px] text-blue-500'>1</p>
+    <p className="font-inter text-lg font-bold leading-6 tracking-tighter ">Real-time Tracking </p>
+    <p className="paragraph font-cabin text-sm font-normal leading-5 tracking-wider ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum nulla praesentium vel suscipit debitis.</p>
+   </div>
+   <div className=" text-white md:text-right space-y-2">
+    <p className='flex items-center justify-center rounded-full bg-[#FFFFFF] md:ml-[210px] h-[54px] w-[54px] text-blue-500'>2</p>
+    <p className="text-white font-inter text-lg font-bold leading-6 tracking-tighter ">Ride Options</p>
+    <p className='paragraph font-cabin text-sm font-normal leading-5 tracking-wider'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum nulla praesentium vel suscipit debitis.</p>
+   </div>
+   </div>
+
+   <div className="relative flex justify-center items-center h-[400px] md:w-1/3">
+  <div className="h-[350px] w-[350px] bg-cyan-600 rounded-full"></div>
+  <div className='flex justify-center items-center  absolute inset-0 z-10'>
+  <img src='https://s3-alpha-sig.figma.com/img/9868/be48/331c1d92f8498912b19a3a8255376b1c?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LZdIWGjH5zP~FSXqrru5hKiSgVhuYfFk6qEcefkZ-ElA4vUadZV1PJCCXrQ18T5LCEI5BTRQeaMH3qqTv6vGeAfigsogUt7QB3F-u7lllpBhGGXoPlHlZ8FLbwDhgbu6f9qdMwx~0MfF2VCUN5emCfW1acc7CGRdSJ1cS8ETAE5rk9pTpxuvSadskFe~zt6202vInkUNesnf5Uao1kB1rqDplMazyoohooVQdarZNm2bMCvdP8BR~Lfz~ASwQAmNd3pBSiIRmUXqq42tbAlfwu0iC7MBnz7XBioPI1jDLAd~~81Pdym4bU1a0jSwDOlgA6A6Gqehwa~ODJlw3-WfDA__' 
+ alt='iphone ss ' 
+ className='border h-[400px] w-[200px] rounded-[32px]'>
+ </img>
+ </div>
+
+   </div>
+
+   <div className="space-y-4  md:w-1/3">
+   <div className=" text-white text-left space-y-2">
+    <p className='flex items-center justify-center rounded-full bg-[#FFFFFF] h-[54px] w-[54px] text-blue-500'>3</p>
+    <p className="font-inter text-lg font-bold leading-6 tracking-tighter">Secure Payments</p>
+    <p className='paragraph font-cabin text-sm font-normal leading-5 tracking-wider'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum nulla praesentium vel suscipit debitis.</p>
+   </div>
+   <div className=" text-white text-left space-y-2">
+    <p className='flex items-center justify-center rounded-full bg-[#FFFFFF] h-[54px] w-[54px] text-blue-500'>4</p>
+    <p className= "font-inter text-lg font-bold leading-6 tracking-tighter ">Driver Ratings</p>
+    <p className='paragraph font-cabin text-sm font-normal leading-5 tracking-wider'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum nulla praesentium vel suscipit debitis.</p>
+   </div>
+   </div>
+
+   </div>
+
+  </div>
+
+{/* ------------------------------------------------------------------------------------------------------------ */}
+
+<div className="bg-[#0C487B] min-h-[680px] border">
+ 
+ <div className="text-white md:flex mx-8 min-h-[80px] mt-16  ">
+  <p className="md:w-1/2 font-inter text-2xl font-bold leading-12 tracking-tighter text-left">Creating valuable content for creatives all the world</p>
+  <p className="md:w-1/2 font-inter text-base font-normal leading-7 text-left">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+    Unde eligendi iure, illum consequuntur sunt numquam impedit, 
+    corporis laudantium praesentium rem aperiam nam maxime quas? Soluta quibusdam corporis.</p>
+ </div>
+
+
+ <div className="md:flex  ">
+  <img src="https://s3-alpha-sig.figma.com/img/b20d/1fae/0c44ef32b6d457f2035c1f89df8b1358?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ONsZhf02lWR2DGcRRhq3cr8boianETv58Q4~UnLx18R9hxqwG2vcPdW0E7X0RsMBLdguSOsKOJ9xSgLUTX80VMCqq5pGMLMHEPSuFCFrC7jBBGfQN-4MtMz2s-7G3Zlj97NzVVnj5G4ojqzPvh2O5j8YjdazWwuxZfmJJG4fv-c5fumIp1TFUFDX-S6vemFees7QeZpsgPwpwuavieMbCaGDmi3munIJNL0Vr6o6wGbKLkBUUt6zJZtiCwSHUlYdFC7u~mDlgIeY5D4oZLj~F-c4SxrIov5O6UE7TItAIpNi~sO~9w2kiqxQFIMHgZaapy9pQdW-b-XJyXbhYpmpXA__"
+   alt="Avatar" 
+   className="md:w-1/3 "></img>
+
+  <img src="https://s3-alpha-sig.figma.com/img/b20d/1fae/0c44ef32b6d457f2035c1f89df8b1358?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ONsZhf02lWR2DGcRRhq3cr8boianETv58Q4~UnLx18R9hxqwG2vcPdW0E7X0RsMBLdguSOsKOJ9xSgLUTX80VMCqq5pGMLMHEPSuFCFrC7jBBGfQN-4MtMz2s-7G3Zlj97NzVVnj5G4ojqzPvh2O5j8YjdazWwuxZfmJJG4fv-c5fumIp1TFUFDX-S6vemFees7QeZpsgPwpwuavieMbCaGDmi3munIJNL0Vr6o6wGbKLkBUUt6zJZtiCwSHUlYdFC7u~mDlgIeY5D4oZLj~F-c4SxrIov5O6UE7TItAIpNi~sO~9w2kiqxQFIMHgZaapy9pQdW-b-XJyXbhYpmpXA__"
+   alt="Avatar" 
+   className="md:w-1/3"></img>
+
+  <img src="https://s3-alpha-sig.figma.com/img/b20d/1fae/0c44ef32b6d457f2035c1f89df8b1358?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ONsZhf02lWR2DGcRRhq3cr8boianETv58Q4~UnLx18R9hxqwG2vcPdW0E7X0RsMBLdguSOsKOJ9xSgLUTX80VMCqq5pGMLMHEPSuFCFrC7jBBGfQN-4MtMz2s-7G3Zlj97NzVVnj5G4ojqzPvh2O5j8YjdazWwuxZfmJJG4fv-c5fumIp1TFUFDX-S6vemFees7QeZpsgPwpwuavieMbCaGDmi3munIJNL0Vr6o6wGbKLkBUUt6zJZtiCwSHUlYdFC7u~mDlgIeY5D4oZLj~F-c4SxrIov5O6UE7TItAIpNi~sO~9w2kiqxQFIMHgZaapy9pQdW-b-XJyXbhYpmpXA__"
+   alt="Avatar" 
+   className="md:w-1/3"></img>
+ </div>
+
+</div>
+
+{/* ------------------------------------------------------------------------------------------------------------- */}
+
+<div className="md:flex bg-[#191919] min-h-[550px] ">
+
+<div className=' flex justify-center items-center md:w-1/2 my-8 '>
+  <div className=" text-white w-[868px] mx-8  ">
+    <p className="font-inter text-3xl md:text-4xl font-bold leading-16 text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p className="font-inter text-base font-medium leading-7 text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Excepturi officiis alias dolore voluptates officia quod aliquid 
+      fugit eaque ut, velit aperiam architecto? Suscipit esse velit assumenda, 
+      pariatur quis, voluptatibus odio molestias provident doloremque debitis 
+      omnis a quod perferendis cumque perspiciatis quisquam impedit culpa tempora atque unde,
+       quam corporis nesciunt alias? Veritatis quis itaque cumque?</p>
+  </div>
+</div> 
+
+  <div className=" flex relative justify-center items-center md:w-1/2 my-8">
+    <div className="h-[300px] w-[300px] rounded-full bg-[#31A0FE]"></div>
+    <div className='flex justify-center items-center absolute inset-0 z-10 '>
+    <img src='https://s3-alpha-sig.figma.com/img/1cf8/3e73/f67a9dadfec2a1230c282bd94d264166?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oTi-FgYYLwzWE8GMiUSa5g5gHLzMJYhk9r3PbA0-X-fZp~SyYgEDI46zHEUk4zPJkCe7jhewaUctSWA8kLL6Ux50edDDWPZaNvEeDqwFfZKtnHrOYpWUjtaw6~g54AI3djg4gQpkujXcHCPnn2K8-3-i5O3hwGeeeXJHYlroBTgY70eH-yrmJRuD-~Gjbh3on5jHoczlr~vTFuMtBXTNBQWvTFByET127mOL7Bl8UFCoQHa14HSPqUsQQ-aB9afQSWeh7BpD1A7-zXaYScxApJE9O5XvBHJa1TJcsKmRt6GFqPtZbJMCipuTwEiKIhL4luNYJ1s4Am5l~uOQ--IXUw__' 
+    alt='' 
+    className='rounded-[31.22px] transform rotate-[22.17deg] h-[330px]'>
+    </img>
+    <img src='https://s3-alpha-sig.figma.com/img/1cf8/3e73/f67a9dadfec2a1230c282bd94d264166?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oTi-FgYYLwzWE8GMiUSa5g5gHLzMJYhk9r3PbA0-X-fZp~SyYgEDI46zHEUk4zPJkCe7jhewaUctSWA8kLL6Ux50edDDWPZaNvEeDqwFfZKtnHrOYpWUjtaw6~g54AI3djg4gQpkujXcHCPnn2K8-3-i5O3hwGeeeXJHYlroBTgY70eH-yrmJRuD-~Gjbh3on5jHoczlr~vTFuMtBXTNBQWvTFByET127mOL7Bl8UFCoQHa14HSPqUsQQ-aB9afQSWeh7BpD1A7-zXaYScxApJE9O5XvBHJa1TJcsKmRt6GFqPtZbJMCipuTwEiKIhL4luNYJ1s4Am5l~uOQ--IXUw__' 
+    alt='' 
+    className='absolute rounded-[31.22px] transform rotate-[-28deg] h-[300px]'>
+    </img>
+    </div>
+  </div>
+</div>
+
+{/* ---------------------------------------------------------------------------------------------------------------- */}
+
+<div className=" bg-[#0C487B] space-y-8  min-h-[750px] border ">
+  <div className="text-white mx-8 space-y-2  min-h-[90px] mt-8">
+    <p className="font-inter text-base font-bold leading-20 tracking-wider text-left">LOREM ISPUM</p>
+    <p className="font-inter text-3xl md:text-5xl font-bold leading-16 text-left">Creating Valuable Content For Creatives All The World</p>
+  </div>
+
+<div className="text-white mx-8 my-4 ">
+  <ul className="space-y-4" >
+    <li className="font-inter text-2xl md:text-3xl font-bold leading-12 tracking-tighter text-left" >Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+    <p className="font-inter text-base font-normal leading-7 text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi minima temporibus cum error libero itaque ratione reiciendis inventore excepturi necessitatibus, explicabo fugit officiis deserunt! At, vel. Quaerat saepe quae corporis alias mollitia culpa commodi quis blanditiis obcaecati ipsam perferendis excepturi quia aut magni, ab harum eligendi nemo facilis velit voluptatem, repellendus deserunt. Voluptatem pariatur porro repudiandae labore dolore ut id, voluptas in vel, magni nemo quo accusantium accusamus mollitia? Voluptatibus deserunt cumque veniam neque. Animi, inventore minima rem dolorem earum ducimus iusto nisi? Vero.</p>
+    
+    <li  className="font-inter text-2xl md:text-3xl font-bold leading-12 tracking-tighter text-left">Lorem ipsum dolor sit, amet consectetur adipisicing.</li>
+    <p className="font-inter text-base font-normal leading-7 text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi minima temporibus cum error libero itaque ratione reiciendis inventore excepturi necessitatibus, explicabo fugit officiis deserunt! At, vel. Quaerat saepe quae corporis alias mollitia culpa commodi quis blanditiis obcaecati ipsam perferendis excepturi quia aut magni, ab harum eligendi nemo facilis velit voluptatem, repellendus deserunt. Voluptatem pariatur porro repudiandae labore dolore ut id, voluptas in vel, magni nemo quo accusantium accusamus mollitia? Voluptatibus deserunt cumque veniam neque. Animi, inventore minima rem dolorem earum ducimus iusto nisi? Vero.</p>
+   
+    <li  className="font-inter text-2xl md:text-3xl font-bold leading-12 tracking-tighter text-left">Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+    <p className="font-inter text-base font-normal leading-7 text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi minima temporibus cum error libero itaque ratione reiciendis inventore excepturi necessitatibus, explicabo fugit officiis deserunt! At, vel. Quaerat saepe quae corporis alias mollitia culpa commodi quis blanditiis obcaecati ipsam perferendis excepturi quia aut magni, ab harum eligendi nemo facilis velit voluptatem, repellendus deserunt. Voluptatem pariatur porro repudiandae labore dolore ut id, voluptas in vel, magni nemo quo accusantium accusamus mollitia? Voluptatibus deserunt cumque veniam neque. Animi, inventore minima rem dolorem earum ducimus iusto nisi? Vero.</p> 
+  </ul>
+  </div>
+
+</div>
+
+{/* ------------------------------------------------------------------------------------------------------------ */}
+<div className="max-w-screen md:grid grid-cols-2 bg-white items-center">
+  <div className="grid-cols-1">
+    <img src="https://s3-alpha-sig.figma.com/img/2f58/b1ab/4704035ad09b7f0130d045eb1774a16e?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=h5aAcRl8bB0Vec4yewwOHnTl~CnqqIx172PHjbtSGN99xjUqmpMt5b-DGBwpq4h-a3JdLpMAlrZhqwdb3GA~9VIJ8OpJOHKozZ44Qqb0HVF0qGzogP6rt1Z49xglyvBGo9y7KHJN6anT55w-rbECvfg8tZAnaLMRy7qSQr0tZTeS7Bs~~qeyXav2OB9hnDd5E2oVUEU3F99eX9gHC6tP8PPmY-tK9zFwt1gqOx38ClBjDT3EuZ1CtCAhuJS9H9OHVIeJSp3tADe2xUYzJEfMKog32I7nCM-NLDG77Sd-X0ZCmGVe9xhvZXuyNPfIOF7jdvI2ZPo7GZxlkE54IIiaJw__"
+    alt=""
+    className=""></img>
+  </div>
+
+  <div className="grid-cols-1 bg-white my-8 mx-8 space-y-8">
+    <div className="border-black  ">
+      <p className="text-2xl md:text-4xl font-poppins font-semibold">Energy of a start-up combine with 30 years of experience.</p>
+    </div>
+    <div className="bg-[#0C487B] border flex justify-center items-center text-white rounded-[31px] h-[54.21px] w-[216px] ">
+    See Job Vacancies
+    </div>
+
+  </div>
+</div>
+
+  
+    </div>
+  )
+}
+
+export default CaseStudy
---
+++ src/content/indus/Indus.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Indus = () => {
+  return (
+    <div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Indus
